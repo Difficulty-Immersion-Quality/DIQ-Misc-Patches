d62469571b3569bc9eb74c989652ea4ab60f9ce0
BaldursGoonsack <149293305+BaldursGoonsack@users.noreply.github.com>
2025-05-29 15:23:12 +1000

    Progression bug fixes: Wild Magic Barb/Druid multiclassing, Arcane Trickster level 10 spellslots

--- DIQ Misc Patches/Mods/DIQ Misc Patches/ScriptExtender/Lua/BootstrapServer.lua
+++ DIQ Misc Patches/Mods/DIQ Misc Patches/ScriptExtender/Lua/BootstrapServer.lua
@@ -8,6 +8,7 @@ Ext.Osiris.RegisterListener("CharacterJoinedParty", 1, "after", function(charID)
     AddSpell(charID, "Shout_DIQ_Utility_Spells_Container", 0, 0);
 end)
 
+-- TODO: Yeet when I do my own mobility overhaul
 Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(level_name, is_editor_mode)
     for i, v in ipairs(Ext.Entity.GetAllEntitiesWithComponent("ServerCharacter")) do
         Ext.Timer.WaitFor(100, function()
